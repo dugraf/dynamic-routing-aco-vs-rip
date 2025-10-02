37c694f7e332d5b50a81b1d7cb4ac1e4ce4eacd4
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2025-10-01 22:09:00 -0300

    Mudanças nos IPs

--- update.ps1
+++ update.ps1
@@ -8,6 +8,6 @@ docker stop router1 router2 router3
 docker rm router1 router2 router3
 
 # Rodar os containers com as configurações específicas
-docker run -d --name router3 --net drp-net --ip 172.20.0.4 -v ${PWD}/config_router3.json:/app/config.json drp-router
-docker run -d --name router2 --net drp-net --ip 172.20.0.3 -v ${PWD}/config_router2.json:/app/config.json drp-router
-docker run -d --name router1 --net drp-net --ip 172.20.0.2 -v ${PWD}/config_router1.json:/app/config.json drp-router
+docker run -d --name router3 --net drp-net --ip 172.20.0.13 -v ${PWD}/config_router3.json:/app/config.json drp-router
+docker run -d --name router2 --net drp-net --ip 172.20.0.12 -v ${PWD}/config_router2.json:/app/config.json drp-router
+docker run -d --name router1 --net drp-net --ip 172.20.0.11 -v ${PWD}/config_router1.json:/app/config.json drp-router
